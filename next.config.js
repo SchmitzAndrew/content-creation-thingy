88f26bbe8dfa99f8a6e38137a19726af9b74530a
SchmitzAndrew <schmitzandrew03@gmail.com>
2023-07-15 19:58:55 -0700

    add hero and prisma schema

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    images: {
+        domains: [
+            "lh3.googleusercontent.com",
+            "static.tryleap.ai",
+            "images.unsplash.com",
+        ],
+    },
+};
 
 module.exports = nextConfig
